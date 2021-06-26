aa122de2cf47359b24be23a27af315921565a311
Jaimie <cruiseomondi90@gmai.com>
2021-06-26 18:36:21 +0300

    second commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const {registerValidation} = require('../validation');
 
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object( {
-    name:Joi.string().min(6).required(),
-    email:Joi.string().min(6).required().email(),
-    password:Joi.string().min(6).required()
-});
 
 router.post('/register',async(req,res)=>{
-    //Validate user before making user
-    const {error} = schema.validate(req.body);
-    res.send(error.details[0].message);
-    // const user = new User({
-    //     name:req.body.name,
-    //     email:req.body.email,
-    //     password:req.body.password
-    // });
-    // try{
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // }catch(err){
-    //     res.status(400).send(err);
-    // }
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    const user = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
 
 router.post('/login',(req,res)=>{
